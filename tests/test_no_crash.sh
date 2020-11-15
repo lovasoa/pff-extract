480e7670eae7416eed83d41528c58baebf772350
ophir <pere.jobs@gmail.com>
2020-11-15 12:43:26 +0100

    add tests for well-formed files

--- tests/test_no_crash.sh
+++ tests/test_no_crash.sh
@@ -2,19 +2,23 @@
 
 errors=0
 total=0
-for f in files/*
+
+for type in "malformed" "correct"
 do
-  echo -e "\n========= Testing '$f' ==========="
-  ../pff-extract "$f" >output.txt 2>&1
-  code=$?
-  tail output.txt
-  rm output.txt
-  echo "Exit code for '$f' : $code"
-  if [ $code -gt 100 ]
-  then
-    errors=$(($errors+1))
-  fi
-  total=$(($total+1))
+  for f in files/$type/*
+  do
+    echo -e "\n========= Testing '$f' ==========="
+    ../pff-extract "$f" >output.txt 2>&1
+    code=$?
+    tail output.txt
+    rm output.txt
+    echo "Exit code for '$f' : $code"
+    if [[ $code -gt 100 || ( $code -ne 0 && $type -eq "correct" ) ]]
+    then
+      errors=$(($errors+1))
+    fi
+    total=$(($total+1))
+  done
 done
 
 echo "$errors/$total test failures."
