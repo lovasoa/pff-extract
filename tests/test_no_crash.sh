f17f405e62b28a3324af7456b07cf086876779b7
ophir <pere.jobs@gmail.com>
2020-11-15 11:39:45 +0100

    Fix testing script

--- tests/test_no_crash.sh
+++ tests/test_no_crash.sh
@@ -4,8 +4,10 @@ final=0
 for f in files/*
 do
   echo -e "\n========= Testing '$f' ==========="
-  ../pff-extract "$f" /dev/null 2>&1 | tail -n 5
+  ../pff-extract "$f" 2>&1 >output.txt
   code=$?
+  tail output.txt
+  rm output.txt
   echo "Exit code for '$f' : $code"
   if [ $code -gt 100 ]
   then
